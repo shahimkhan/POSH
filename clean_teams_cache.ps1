e499bffbd6f907b32fc9c7f8c669737c3823ec6b
Shahim Khan <shahim@outlook.com>
2020-07-15 10:35:30 +1000

    Update to Force killing of Teams Proces

--- clean_teams_cache.ps1
+++ clean_teams_cache.ps1
@@ -1,6 +1,6 @@
 Write-Host "Clearing Teams Disk Cache" -ForegroundColor Yellow
 try{
-Stop-Process -Name Teams ;
+Stop-Process -Name Teams -Force ;
 Write-Host "Teams process killed" -ForegroundColor Green
 
 Get-ChildItem "C:\Users\*\AppData\Roaming\Microsoft\Teams\*" -directory | Where name -in ('application cache','blob storage','databases','GPUcache','IndexedDB','Local Storage','tmp') | ForEach{Remove-Item $_.FullName -Recurse -Force}
